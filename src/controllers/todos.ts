83bce3d41f72e8a2eeb20e718c7a6466f2a7a8bc
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-01-23 21:15:35 +0100

    Adding get, update and delete todos

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -15,3 +15,35 @@ export const createTodo: RequestHandler = (req, res, next) => {
     .status(201)
     .json({ message: "Todo created successuflly", createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.status(200).json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (<{ text: string }>req.body).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.status(200).json({ message: "Updated!", updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "Todo deleted!" });
+};
